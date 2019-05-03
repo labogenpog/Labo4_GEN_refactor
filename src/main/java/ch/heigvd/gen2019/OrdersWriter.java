5ca62923d9d975b921a22baf50b9ddb66964a573
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2019-05-03 11:33:21 +0200

    Deplacement de OrderToString de OrdersWriter a Order

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -12,7 +12,7 @@ public class OrdersWriter {
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
             Order order = orders.getOrder(i);
-            orderToString(sb, order);
+            order.orderToString(sb);
         }
 
         if (orders.getOrdersCount() > 0) {
@@ -20,24 +20,6 @@ public class OrdersWriter {
         }
 
         return sb.append("]}").toString();
-    }
-
-    private void orderToString(StringBuffer sb, Order order) {
-        sb.append("{");
-        sb.append("\"id\": ");
-        sb.append(order.getOrderId());
-        sb.append(", ");
-        sb.append("\"products\": [");
-        for (int j = 0; j < order.getProductsCount(); j++) {
-            sb.append(order.getProduct(j).productToString() );
-        }
-
-        if (order.getProductsCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        sb.append("]");
-        sb.append("}, ");
     }
 
 }
